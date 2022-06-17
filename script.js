cdef63b84201b7a825a9d661c4ed6bbbe0ce2dda
Way-Maker7 <Denis_Sabou@gmx.de>
2022-06-17 16:31:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// const bubble = document.createElement("span");
+// document.body.appendChild(bubble);
+// bubble.classList.add("bubble");
+// //console.log(Math.random() * 100 + 100 + "px");
+
+// const size = Math.random() * 200 + 100 + "px";
+// bubble.style.height = size;
+// bubble.style.width = size;
+// //console.log(size);
+
+// bubble.style.top = Math.random() * 100 + 50 + "%";
+// bubble.style.left = Math.random() * 100 + "%";
+
+// bubble.style.setProperty("--left", Math.random() * 100 + "%");
+
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
+const bubbleMaker = () => {
+    const bubble = document.createElement("span");
+document.body.appendChild(bubble);
+bubble.classList.add("bubble");
+//console.log(Math.random() * 100 + 100 + "px");
+
+const size = Math.random() * 200 + 100 + "px";
+bubble.style.height = size;
+bubble.style.width = size;
+//console.log(size);
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+const plusMinus = Math.random() > 0.5 ? 1 : -1;
+bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+bubble.addEventListener("click", () => {
+    counter++;
+    //console.log(counter);
+    counterDisplay.textContent = counter;
+    bubble.remove();
+});
+
+setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+//bubbleMaker();
+
+setInterval(bubbleMaker, 1000);
+
